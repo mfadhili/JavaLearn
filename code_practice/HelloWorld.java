c788b39d9d3af06f0ece7c5e9eafa5f2436e1b49
mfadhili <53080579+mfadhili@users.noreply.github.com>
2022-02-25 12:02:47 +0300

    Add files via upload

---
+++ code_practice/HelloWorld.java
@@ -0,0 +1,16 @@
+// Filename HelloWorld.java
+// Written by Mfadhili GItau
+// Written on 25th Feb 2022
+
+public class HelloWorld
+{
+	/*
+	this is my first java program
+	prints Hello HelloWorld
+	*/
+	
+	public static void main(String []args)
+	{
+		System.out.println("Hello World"); // prints hello world
+	}
+}
