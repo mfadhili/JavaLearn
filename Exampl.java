ab305862a520f9ddb05a90209fe3dbba508edbbd
mfadhili <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@github.com>
2022-03-23 18:47:29 +0300

    All

---
+++ Exampl.java
@@ -0,0 +1,13 @@
+/*
+ * This is a simple Java program
+ * file: Example.java
+ */
+
+public class Exampl
+{
+    public static void main (String args[])
+    {
+        System.out.println("This is a simple java program");
+
+    }
+}
