7d016ac17276e1360d0e425f35dbfcb8a7477841
mfadhili <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@github.com>
2022-03-23 19:20:01 +0300

    All commit

---
+++ Example2.java
@@ -0,0 +1,21 @@
+/*
+ * This is a simple Java program
+ * file: Example2.java
+ */
+
+public class Example2
+{
+    public static void main(String args[])
+    {
+        int num;
+        
+        num = 100;
+        
+        System.out.println("This is num: " + num);
+        
+        num = num*2;
+        
+        System.out.println("The value of num * 2 is : ");
+        System.out.println(num);
+    }
+}
---
+++ gitpusher.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+git add .
+git commit -m "All commit"
+git push -u origin learn
