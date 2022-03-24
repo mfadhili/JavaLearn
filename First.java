f60b999e4227b44669274020f1f874dfae509a3d
mfadhili <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@github.com>
2022-03-24 15:18:19 +0300

    All commit

---
+++ First.java
@@ -0,0 +1,15 @@
+public class First
+{
+    public static void main(String[] args)
+    {
+     nameAndAddress();
+     System.out.println("First Java Application");
+    }
+    
+    public static void nameAndAddress()
+    {
+     System.out.println("XYZ COmpany");
+     System.out.println("8900 U.S. Hwy 14");
+     System.out.println("Crystal Lake IL 60014");
+    }
+}
